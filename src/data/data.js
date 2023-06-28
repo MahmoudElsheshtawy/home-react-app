b5f255983557a40727311c56ca943573df3a003e
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-06-28 00:32:40 -1200

    dev

--- src/data/data.js
+++ src/data/data.js
@@ -30,6 +30,35 @@ export const data=[
         github:"",
         live:"",
     },
+    
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
 
 
 ]
