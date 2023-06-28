af7f5718f85240eb575fd6ae68fa24688b95c987
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-06-28 00:21:05 -1200

    src

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -4,17 +4,23 @@ import Video from '../../assets/Video.mp4'
 const About = () => {
   return (
     <div className="about">
-        <div className="contanier">
+        <div className="connier">
+
             <div data-aos="fade-down"className="left">
+
               <h2>Wonderfull House<br/>experience nin three!</h2>
+
               <p>the grte subscripe is bosde on th trager 
+
                 wingrteu coptjjfs jyiiop countiry
                 the grte subscripe is bosde on th trager wingrteu coptjjfsthe grte subscripe is bosde<br/> on th trager
                  wingrteu coptjjfs jyiiop countiry jyiiop countiry
               </p>
             </div>
             <div data-aos="fade-up" className="right">
+
                 <video autoPlay loop muted id='video'>
+                  
                     <source src={Video} type='Video/mp4'/>
                 </video>
             </div>
--- src/component/hinking/Hinking.jsx
+++ src/component/hinking/Hinking.jsx
@@ -6,21 +6,28 @@ import './Hinking.css'
 const Hinking = () => {
   return (
     <div data-aos="fade-up" className='hinking'>
+
         <h1>Why Hinking?</h1>
+
         <div className="cards">
+          
             <div className="cardss">
+
                        <div className="card">
+
                         <img src={clienttow} alt="" />
                         <h2>110+ Mountains</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga  
                             modi earum ab itaque?earum ab itaque? Suscipit  consequuntur.</p>
                        </div>
+
                        <div className="card">
                         <img src={clientone} alt="" />
                         <h2>110+ Mountains</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga nam  
                             modi earum ab itaque?earum ab itaque? Suscipit consequuntur.</p>
                        </div>
+
                         <div className="card">
                             <img src={clientthree} alt="" />
                             <h2>110+ Mountains</h2>
